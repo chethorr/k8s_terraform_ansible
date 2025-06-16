1b57e80830c1c73494ce0f4df93cece6687285c0
chethorr <chemareedu@gmail.com>
2025-06-17 00:30:52 +0530

    update tf

--- prod_grade_struct/accounts/qa/inventory.ini.tpl
+++ prod_grade_struct/accounts/qa/inventory.ini.tpl
@@ -4,3 +4,6 @@ ${master_node_ip} ansible_port=22 ansible_user=ec2-user ansible_ssh_private_key_
 [worker]
 ${worker_node1_ip} ansible_port=22 ansible_user=ec2-user ansible_ssh_private_key_file=/home/ec2-user/k8s_terraform_ansible/ansible/key
 ${worker_node2_ip} ansible_port=22 ansible_user=ec2-user ansible_ssh_private_key_file=/home/ec2-user/k8s_terraform_ansible/ansible/key
+
+[nfs-server]
+${nfs_server} ansible_port=22 ansible_user=ec2-user ansible_ssh_private_key_file=/home/ec2-user/k8s_terraform_ansible/ansible/key
--- prod_grade_struct/accounts/qa/template.tf
+++ prod_grade_struct/accounts/qa/template.tf
@@ -4,9 +4,9 @@ resource "local_file" "ansible_inventory" {
       master_node_ip  = "${module.cluster.master_cluster_ec2_ip}"
       worker_node1_ip = "${module.cluster.worker1_cluster_ec2_ip}"
       worker_node2_ip = "${module.cluster.worker2_cluster_ec2_ip}"
-      ansible_ec2_ip  = "${module.ansible.ansible_ec2_ip}"
-      nfs_ec2_ip      = "${module.nfs.nfs_ec2_ip}"
+      nfs_server = "${module.nfs.nfs_ec2_mod.public_ip}"
     }
   )
   filename = "${path.module}/inventory.ini"
+  depends_on = [ module.cluster ]
 }
